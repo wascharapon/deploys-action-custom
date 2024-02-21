aed187ebe45ef5c3edbf2891e3a6ef550623b431
MR.WASCHARAPON AKKHAHAD <games_big@hotmail.co.th>
2024-02-21 18:21:36 +0700

    refactor:deploy version 52

--- dist/index.js
+++ dist/index.js
@@ -8264,6 +8264,7 @@ var axiosConfigDeployApp = {
 	url: '',
 	headers: {
 		'content-type': 'application/json',
+		Authorization: 'Basic'
 	},
 	auth: {}
 };
@@ -8585,10 +8586,7 @@ async function run() {
 
 		const deployHandler = new DeployHandler()
 
-		axiosConfigDeployApp.auth = {
-			username: 'github@scamo-group.serviceaccount.deploys.app',
-			password: 'Fm96uItZ2nuN5BzwCGrFCoifLfLcfnYUzVJJNtrn7Ord4HB4rwzv8W25XgXg4TBr'
-		}
+		axiosConfigDeployApp.headers.Authorization = 'Basic ' + Buffer.from(inputs.unameDeployApp + ':' + inputs.passDeployApp).toString('base64')
 
 		masterDeployAppBodyRequest.port = Number(inputs.portDeployApp)
 
