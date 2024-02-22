a9b6d84454127150dfc2538b13ddc87115504260
MR.WASCHARAPON AKKHAHAD <games_big@hotmail.co.th>
2024-02-22 11:59:10 +0700

    refactor:version deploy v57

--- src/main.js
+++ src/main.js
@@ -337,12 +337,12 @@ async function run() {
 		const deployHandler = new DeployHandler()
 
 		axiosConfigDeployApp.auth = {
-			username: process.env.DEPLOYS_AUTH_USER,
-			password: process.env.DEPLOYS_AUTH_PASS
+			username: inputs.DEPLOYS_AUTH_USER,
+			password: inputs.DEPLOYS_AUTH_PASS
 		}
-		
+
 		core.info(`VIEW ENV : ${JSON.stringify(process.env)}`)
-		core.info(`TEST ENV : ${process.env.DEPLOYS_AUTH_PASS}`)
+		core.info(`TEST ENV : ${process.env.DEPLOYS_AUTH_TEST}`)
 
 		masterDeployAppBodyRequest.port = Number(inputs.portDeployApp)
 
